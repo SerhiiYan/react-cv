0899ffc21f78d9337786f4ac6415761282827087
YanchevskyiSerhii <serhii.yanchevskii@gmail.com>
2022-07-06 22:50:49 +0300

    feat: add resume

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ function App() {
           <div className="home">
             <h1>I'm Serhii Yanchevskyi</h1>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Distinctio, eveniet corrupti corporis praesentium deserunt magni
-              fugiat unde nisi alias repellendus voluptates eum veritatis.
-              Consequuntur fuga, ea impedit earum architecto exercitationem.
+              Hello everyone, for about a year I have been studying html, css,
+              js with a big bias towards js and react (now I am studying react,
+              redux). During this time, I took many online courses, such as:
+              Ultimate Courses by Todd Motto, FreeCodeCamp, LearnJs
             </p>
             <div>
               <a href="#">
@@ -67,6 +67,65 @@ function App() {
                 fugiat unde nisi alias repellendus voluptates eum veritatis.
                 Consequuntur fuga, ea impedit earum architecto exercitationem.
               </p>
+            </div>
+          </div>
+          <div className="resume">
+            <div className="work left">
+              <div>
+                <h2>Work experience</h2>
+                <h3>Software Maintenance Specialist</h3>
+                <p>Сompany «Kvant» 2011 - 2013 (Ukraine)</p>
+                <ul>
+                  <li>
+                    Serviced the accounting program M.E. doc (not related to
+                    programming or testing)
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h3>Operator of electronic typing and layout</h3>
+                <p>Company «Grodno Azot» 2015 - 2018</p>
+                <ul>
+                  <li>
+                    Worked on such equipment: HEIDELBERG Suprasetter, mimaki
+                    jv300-160{" "}
+                  </li>
+                  <li>Designed from large stands to small business cards</li>
+                  <li>Printed large format images and much more. </li>
+                </ul>
+              </div>
+              <div>
+                <h3>Social Media Evaluator</h3>
+                <p>Company «Appen» 2018 - 2022</p>
+                <ul>
+                  <li>
+                    Feedback on news feeds, advertisements and search results on
+                    social networks such as facebook and instagram
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="right">
+              <div className="education">
+                <h2>Education</h2>
+                <p>Ukrainian National Forestry University in Lviv</p>
+
+                <ul>
+                  <li>Bachelor in Management</li>
+                  <li>2010 - 2015</li>
+                </ul>
+              </div>
+              <div className="skills">
+                <h2>Skills</h2>
+                <h3>Programming languages:</h3>
+                <ul>
+                  <li>HTML/CSS, JavaScript, ReactJS</li>
+                </ul>
+                <h3>Development environments:</h3>
+                <ul>
+                  <li>Visual Studio, Git</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
